6227aa7804a87973e3842506d8e497446b3c5612
Kulkarni <sk2712@csu.fullerton.edu>
2024-03-25 15:05:48 -0700

    s3 config updated

--- app.py
+++ app.py
@@ -1,51 +1,51 @@
-import numpy as np
-import scipy as sp
+import boto3
 import pandas as pd 
-import os
-# import matplotlib.pyplot as plt
-from flask import Flask,request,render_template
+
+from flask import Flask, request, render_template
 import pickle
 
 app = Flask(__name__)
-model = pickle.load (open ('model.pkl','rb'))
+s3 = boto3.client('s3')
+model = pickle.load(open('model.pkl', 'rb'))
 
-@app.route ('/')
+@app.route('/')
 def home():
-    return render_template ('index.html')
+    return render_template('index.html')
 
-@app.route('/predict',methods=['POST','GET'])
+def get_file_url(bucket_name, file_key):
+    url = s3.generate_presigned_url(
+        ClientMethod='get_object',
+        Params={
+            'Bucket': bucket_name,
+            'Key': file_key
+        },
+        ExpiresIn=3600  
+    )
+    return url
+
+@app.route('/predict', methods=['POST'])
 def predict():
     file = request.files['file']
-    file.save(os.path.join('C:/Users/SK2712/OneDrive - Cal State Fullerton/Desktop/FinalProj/finalProject', file.filename))
-    df = pd.read_csv(file.filename)
+    if not file:
+        return 'No file provided in the request'
 
-    # df['Attrition'].replace('Yes','1',inplace=True)
-    # df['Attrition'].replace('No','0',inplace=True) 
-    df.drop(['EmployeeCount','Over18','StandardHours','EmployeeNumber'],axis=1,inplace=True)
+    file.save(file.filename)
+    bucket_name = 'employeeattri1'  
+    s3.upload_file(file.filename, bucket_name, file.filename)
+
+    df = pd.read_csv(file.filename)
+    df.drop(['EmployeeCount', 'Over18', 'StandardHours', 'EmployeeNumber'], axis=1, inplace=True)
 
     for col in df.columns:
         if df[col].dtype == "object":
             df[col] = df[col].astype('category')
             df[col] = df[col].cat.codes
 
-    # X = df.drop(['Attrition'], axis=1)
     X = df
-
     prediction = model.predict(X)
 
-    print(prediction)
-
-    count= np.count_nonzero(prediction == 1)
+    count = sum(prediction)
     co = len(prediction)
-    for i in range(0, len(prediction)):  
-        print(prediction[i]),
-    print(count)
-    print(co)
-    p = count / co * 100
+    p = count / co * 100 if co > 0 else 0
 
-    if prediction.any():
-        return render_template('show.html',p=p,prediction_text='Predicted Sucessfully')
-    else:
-        return render_template ('index.html',prediction_text='Predicted Failed')
-
-           
+    return render_template('show.html', p=p, prediction_text='Predicted Successfully')
